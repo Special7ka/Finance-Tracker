f573ded9935e4fb9f18ed4643c082cf0963de2e3
Illia <ilaryndin30@gmail.com>
2026-03-07 23:49:29 +0100

    feat: implement PATCH /categories/:id with validation and unique name check

--- server/src/routes/categories.ts
+++ server/src/routes/categories.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/verifyJWT";
 import { authorization } from "../middlewares/parseAuthorization";
-import { getCategoriesByUserId } from "../services/categories.service";
+import { getCategoriesByUserId, updateCategory } from "../services/categories.service";
 
 const router = Router();
 
@@ -9,6 +9,20 @@ router.get("/",authorization, verifyJWT, async(req, res) =>{
     const userId = (req as any).userId
     const categories = await  getCategoriesByUserId(userId)
     res.json(categories)
+})
+
+router.patch("/:id",authorization,verifyJWT, async(req, res) =>{
+    const userId = (req as any).userId
+    const categoryID = Array.isArray(req.params.id) ? req.params.id[0] : req.params.id
+    const name = req.body.name
+
+    if(!name || name.trim() === ""  ){
+        return res.status(400).json({error:"invalid name"})
+    }
+
+    const newCategory  = await updateCategory(userId, categoryID, name)
+    
+    return  res.status(200).json({message: "successfully updated", category: newCategory})
 })
 
 export default router
--- server/src/services/categories.service.ts
+++ server/src/services/categories.service.ts
@@ -9,3 +9,33 @@ export async function getCategoriesByUserId(userId: string) {
 
     return userCategories;
 }
+
+export async function updateCategory(userId: string, categoryID: string, newName: string){
+    const prisma = getPrisma();
+
+    const userCategory = await prisma.category.findFirst({
+        where:{id: categoryID, userId: userId}
+    })
+    if(!userCategory){
+        throw new Error("Category not found")
+    }
+    if(userCategory?.name === newName){
+        return userCategory;
+    }
+    
+
+    const exists = await prisma.category.findFirst({
+        where:{
+            userId:userId,
+            name: newName,
+            NOT: {id:categoryID}
+        }
+    })
+    if(exists){
+        throw new Error("Category already exist")
+    }
+
+    const newCategory =  await prisma.category.update({where:{id:categoryID}, data:{name: newName}})
+
+    return newCategory;
+}
