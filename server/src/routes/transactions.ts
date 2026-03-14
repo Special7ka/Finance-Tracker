c3511a8ec45dd950ab6429b01f2e4aaac2dd4022
Illia <ilaryndin30@gmail.com>
2026-03-14 21:28:55 +0100

    feat: implement POST /transactions

---
+++ server/src/routes/transactions.ts
@@ -0,0 +1,57 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/verifyJWT";
+import { authorization } from "../middlewares/parseAuthorization";
+import { getTransactions,createTransaction,updateTransaction,deleteTransaction } from "../services/transactions.service";
+import { json } from "node:stream/consumers";
+
+const router = Router()
+
+router.post("/",authorization,verifyJWT,async(req,res)=>{
+    const userId = (req as any).userId
+    const amount = req.body.amount
+    const occurredAt = req.body.occurredAt
+    const occurredDate = new Date(occurredAt)
+    const categoryId = req.body.categoryId
+    const type = req.body.type
+
+    if(!amount || typeof(amount) !== "number" || amount <= 0 ){
+        return res.status(400).json({error: "invalid amount"})
+    }
+    if(
+        !type || 
+        typeof(type) !== "string" || 
+        (type !== "INCOME"  && type !== "EXPENSE")|| 
+        type.trim() === ""
+    )
+    {
+        return res.status(400).json({error: "invalid type"})
+    }
+    if(Number.isNaN(occurredDate.getTime()) ){
+        return res.status(400).json({error: "invalid occurredAt"})
+    }
+    if(categoryId){
+        if(typeof(categoryId) !== "string" || categoryId.trim() === ""){
+            return res.status(400).json({error: "invalid category id"})
+        }
+    }
+
+    try{
+        const transaction = await createTransaction(userId,type,occurredDate,amount,categoryId)
+        return res.status(201).json({transaction})
+    }catch(e){
+        if(e instanceof Error){
+            res.status(400).json({error: e.message})
+        }
+    }
+})
+router.get("/",authorization,verifyJWT,async(req,res)=>{
+    
+})
+router.patch("/:id",authorization,verifyJWT,async(req,res)=>{
+    
+})
+router.delete("/:id",authorization,verifyJWT,async(req,res)=>{
+    
+})
+
+export default router
--- server/src/services/transactions.service.ts
+++ server/src/services/transactions.service.ts
@@ -1,8 +1,29 @@
 import { getPrisma } from "../db/prisma";
 import { TransactionType } from "@prisma/client";
 
-export async function createTransaction(userId:string,type: TransactionType,occurredAt: Date, categoryId:string, amount: number) {
+export async function createTransaction(userId:string,type: TransactionType,occurredAt: Date,amount: number, categoryId?:string, ) {
     const prisma = getPrisma()
+    
+
+    if(categoryId){
+        const category = await prisma.category.findUnique({where:{
+        id:categoryId
+        }})
+
+        if(userId !==category?.userId){
+            throw new Error("category not found")
+        }
+    }
+    
+    const newTransaction = await prisma.transaction.create({data:{
+        userId:userId,
+        type:type,
+        occurredAt:occurredAt,
+        categoryId:categoryId,
+        amount:amount,
+    }})
+
+    return newTransaction;
 }
 export async function getTransactions(userId:string,categoryId:string ) {
     const prisma = getPrisma()
