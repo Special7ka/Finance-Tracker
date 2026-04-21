3d94beb70df71310cfe601a94f1512f49ebe0245
Ilia Ryndin <ilaryndin30@gmail.com>
2026-04-21 18:53:33 +0200

    feat(frontend-auth): implement login page with fake token handling

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,3 +1,38 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
+
 export const LoginPage = () => {
-  return <></>
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const navigate = useNavigate()
+  const token = localStorage.getItem('token')
+
+  if (token) {
+    return <Navigate to="/transactions" replace />
+  }
+  return (
+    <>
+      Login
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          localStorage.setItem('token', 'some-token-code')
+          navigate('/transactions', { replace: true })
+        }}
+      >
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />{' '}
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit"> Submit</button>
+      </form>
+    </>
+  )
 }
