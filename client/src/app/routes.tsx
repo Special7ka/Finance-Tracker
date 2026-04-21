afbdf754391b9a7a77cc90f74f5853d441016cb0
Ilia Ryndin <ilaryndin30@gmail.com>
2026-04-21 17:49:30 +0200

    feat(frontend-auth): add protected routes and redirect logic

--- client/src/app/routes.tsx
+++ client/src/app/routes.tsx
@@ -3,15 +3,17 @@ import { LoginPage } from "../pages/Login";
 import { RegisterPage } from "../pages/Register";
 import { TransactionsPage } from "../pages/Transactions";
 import { NotFoundPage } from "../pages/NotFoundPage";
+import { ProtectedRoute } from "../components/ProtectedRoute";
 
 export const AppRoutes = () => {
+    const token = localStorage.getItem("token")
   return (
     <Routes>
-      <Route path = "/" element = {<Navigate to = "/login" />}></Route>
+      <Route path = "/" element = {token ?   <Navigate to = "/transactions" replace/> : <Navigate to ="/login" replace/>  }/>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
-      <Route path="/transactions" element={<TransactionsPage />} />
-      <Route path="/*" element={<NotFoundPage />} />
+      <Route path="/transactions" element={<ProtectedRoute> <TransactionsPage /></ProtectedRoute>} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   )
 }
---
+++ client/src/components/ProtectedRoute.tsx
@@ -0,0 +1,14 @@
+import { Navigate } from "react-router-dom"
+import type { ReactNode } from "react"
+
+type props = {
+    children: ReactNode
+}
+export const ProtectedRoute = ({children}:props) =>{
+    const token = localStorage.getItem("token")
+    if(!token) {
+        return <Navigate to = "/login" />
+    }
+
+    return children
+}
