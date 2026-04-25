9dfd0db4fd919365ca271eebfcd3e93b1ee263cc
Ilia Ryndin <ilaryndin30@gmail.com>
2026-04-25 23:55:26 +0200

    feat(transactions): add transaction form with categories select and API integration

---
+++ client/src/features/transactions/TransactionsForm.tsx
@@ -0,0 +1,85 @@
+import { useEffect, useState } from 'react'
+import { api } from '../../api/client'
+
+type Category = {
+  id: string
+  name: string
+}
+
+export const TransactionsForm = () => {
+  const [amount, setAmount] = useState('')
+  const [type, setType] = useState('')
+  const [categoryId, setCategoryId] = useState('')
+  const [occurredAt, setOccurredAt] = useState('')
+  const [categories, setCategories] = useState<Category[]>([])
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const res = await api.get('/categories')
+        setCategories(res.data.categories)
+      } catch (e) {
+        console.log(e)
+      }
+    }
+
+    fetchCategories()
+  }, [])
+
+  return (
+    <form
+      onSubmit={async (e) => {
+        e.preventDefault()
+
+        try {
+          if (!amount || !type || !occurredAt) {
+            alert('Fill required fields')
+            return
+          }
+
+          await api.post('/transactions', {
+            amount: Number(amount),
+            type,
+            categoryId: categoryId || null,
+            occurredAt,
+          })
+        } catch (e) {
+          console.log(e)
+        }
+      }}
+    >
+      <select value={type} onChange={(e) => setType(e.target.value)}>
+        <option value="">Select type of transaction</option>
+        <option value="EXPENSE">Expense</option>
+        <option value="INCOME">Income</option>
+      </select>
+
+      <select
+        value={categoryId}
+        onChange={(e) => setCategoryId(e.target.value)}
+      >
+        <option value="">Select category</option>
+
+        {categories.map((category) => (
+          <option key={category.id} value={category.id}>
+            {category.name}
+          </option>
+        ))}
+      </select>
+
+      <input
+        type="number"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+      />
+
+      <input
+        type="date"
+        value={occurredAt}
+        onChange={(e) => setOccurredAt(e.target.value)}
+      />
+
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
--- client/src/pages/TransactionsPage.tsx
+++ client/src/pages/TransactionsPage.tsx
@@ -1,8 +1,10 @@
 import { useNavigate } from 'react-router-dom'
+import { TransactionsForm } from '../features/transactions/TransactionsForm'
 export const TransactionsPage = () => {
   const navigate = useNavigate()
   return (
     <>
+      <TransactionsForm />
       <button
         onClick={() => {
           localStorage.clear()
